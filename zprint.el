269a70d103d5714cb42f1fd40100bd9e8d2b005a
tianshu <doglooksgood@gmail.com>
2021-06-13 18:59:35 +0800

    add zprint.el

---
+++ zprint.el
@@ -0,0 +1,78 @@
+;;; zprint.el --- Zprint in Emacs  -*- lexical-binding: t; -*-
+
+;; Author: Shi Tianshu
+;; Keywords: convenience, modal-editing
+;; Package-Requires: ((emacs "27.1"))
+;; Version: 0.0.1
+;; URL: https://www.github.com/DogLooksGood/zprint.el
+;;
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; Format Clojure buffer with zprint(binary).
+;; How to use:
+;; 1. Manually format via shortcut:
+;;
+;;     (define-key clojure-mode-map (kbd "C-c p") 'zprint)
+;;
+;; 2. Automatically format before save:
+;;
+;;     (add-hook 'clojure-mode-hook 'zprint-mode)
+;;
+;;; Code:
+
+(defvar zprint-bin-path "zprint"
+  "The path to zprint binary.")
+
+(defvar zprint-lighter " zp"
+  "The lighter displayed in modeline.")
+
+(defun zprint ()
+  "Reformat code using zprint."
+  (interactive)
+  (let* ((contents (buffer-string))
+         (ln (line-number-at-pos (point) t))
+         (col (- (point) (line-beginning-position)))
+         (formatted-contents
+          (with-temp-buffer
+            (insert contents)
+            (let ((retcode (call-process-region
+                            (point-min)
+                            (point-max)
+                            zprint-bin-path
+                            t
+                            (current-buffer)
+                            nil)))
+              (if (zerop retcode)
+                  (buffer-string)
+                (error "zprint failed: %s" (string-trim-right (buffer-string))))))))
+    (erase-buffer)
+    (insert formatted-contents)
+    (goto-char (point-min))
+    (forward-line (1- ln))
+    (forward-char col)))
+
+(define-minor-mode zprint-mode
+  "Format code before save."
+  :lighter zprint-lighter
+  (if zprint-mode
+      (add-hook 'before-save-hook 'zprint nil t)
+    (remove-hook 'before-save-hook 'zprint t)))
+
+(provide 'zprint)
